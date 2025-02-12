520dbd4d65366da13dc68e1c967b3220850499dd
flytrap <f20240755@pilani.bits-pilani.ac.in>
2025-02-12 23:56:59 +0530

    Updated file with latest changes

--- hamburger.js
+++ hamburger.js
@@ -3,22 +3,17 @@ const closeBtn = document.getElementById("closeBtn");
 const sideMenu = document.getElementById("sideMenu");
 const header = document.getElementById("navbar");
 
+sideMenu.style.left = "-80%";
+
 menuBtn.addEventListener("click", () => {
-  sideMenu.style.left = "0";
+  sideMenu.style.left = "0%";
 });
 
 closeBtn.addEventListener("click", () => {
-  sideMenu.style.left = "-700px";
+  sideMenu.style.left = "-80%";
 });
 
 document.addEventListener("click", (event) => {
-  if (!sideMenu.contains(event.target) && !menuBtn.contains(event.target)) {
-    sideMenu.style.left = "-700px";
-  }
-});
-
-
-//to make the header stick to the top when scrolling up and not when we are scrolling down
-navbar.addEventListener("mouseup", () => {
-  navbar.style.down = "50px";
+  if (!sideMenu.contains(event.target) && !menuBtn.contains(event.target)) 
+    sideMenu.style.left = "-80%";
 });
